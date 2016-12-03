ed0ba2e2ed427038026c8c23d4f31fce944fdbe6
obinna Igbe <obiigbe91@gmail.com>
2016-12-03 00:57:26 -0500

    after finish working on NsaConstantDetectorClassifier

--- myclassifiers.py
+++ myclassifiers.py
@@ -1,25 +1,133 @@
 from sklearn.base import BaseEstimator
 from sklearn.base import ClassifierMixin
 from sklearn.preprocessing import LabelEncoder
+from sklearn.utils.validation import check_X_y,check_array,check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KDTree
+import random
+import numpy as np
 
-class NsaConstantDetectorClassifier(BaseEstimator,ClassifierMixin):
+class NsaConstantDetectorClassifier (BaseEstimator,ClassifierMixin):
 
 
-    def __init__(self,number_of_detectors = 1000, self_radius_size = 0.16, random_gen_param = (0.0001,1.0), otherParam=None):
+    def __init__(self,number_of_detectors = 1000, self_radius_size = 0.16, random_gen_param = (0.0001,1.0),
+                 class_label = (1.0,2.0),otherParam=None):
         """
 
         :param number_of_detectors: int value of the number of detector you will like to generate
         :param self_radius_size:  the self radius of the normal samples
         :param random_gen_param: a tuple for the random generator use for generating initial detector positions or points
             default is is (0.0001,1) which mean generator range will start from very small values. Note also that this
-             can cause the random points to be more distributed towards more small values.
+             can cause the random points to be more distributed towards more small values. The final value 1 means that
+             the maximum value of the search space is 1 and 0.0001 means that the minimum value is 0.0001.
+        :param class_label:this is a tuple containing the two possible labels found in the dataset. To be supplied.
         :param otherParam: not used for now
         """
 
         self.number_of_detectors = number_of_detectors
         self.self_radius_size = self_radius_size
         self.random_gen_param = random_gen_param
+        self.class_label = class_label
+
+    def fit(self,X,y):
+        """
+
+        :param X: this is a numpy array of the self_set without the class_label
+        :param y: array-like, shape= [n_samples]. this is the vector of target class labels
+        :return: it returns a self object
+        """
+
+        #To check and ensure that the parameter types correspond to what is needed or expected, we run a type check
+
+        if type(self.number_of_detectors) != int:
+            raise Exception("number_of_detectors must be an integer value")
+        elif type(self.self_radius_size) != float:
+            raise Exception ('self_radius_size must be an floating pont value')
+        elif type(self.random_gen_param) != tuple:
+            raise Exception ('random_gen_param must be a tuple value')
+        elif type(X) != np.ndarray:
+            raise Exception ('self list X must be a numpy array')
+        elif type(y) != np.ndarray:
+            raise Exception ('class label y must be a numpy array')
+
+        #To encode the label, to ensure that the class lebel starts with 0, which is important for np.argmax
+        self.class_lablenc_ = LabelEncoder()
+        self.class_lablenc_.fit(y)
+        self.classes = self.class_lablenc_.classes_
+
+        #to store unique classes you do
+        self.classes_ = unique_labels(y)
+        #print self.classes_
+
+        #To find the number of columns in the self_set matrix whic will help use generate same dimension of random detectors
+        #X = np.array(X)
+        dimension = np.shape(X)[1]
+        min = self.random_gen_param[0]
+        max = self.random_gen_param[1]
+
+        #this list is only used here inside this class hence, the "_" at the end of the detector_list name
+
+        self.detector_list_=[]
+        kdt = KDTree(X, leaf_size=30, metric='euclidean')
+
+        while len(self.detector_list_) < self.number_of_detectors:
+            #print len(self.detector_list_)
+            new_random_detector = [10 ** random.uniform(np.log10(min), np.log10(max)) for i in range(dimension)]
+
+            dist, ind = kdt.query([new_random_detector], k=1)
+            #print 'first x distance: ', dist
+
+            if dist > self.self_radius_size:
+                # print 'second d distance: ',dist
+                if len(self.detector_list_) == 0:
+                    self.detector_list_.append(new_random_detector)
+                else:
+                    kdt = KDTree(self.detector_list_, leaf_size=30, metric='euclidean')
+                    dist2, ind = kdt.query([new_random_detector], k=1)
+                    if dist2 > self.self_radius_size:
+                        self.detector_list_.append(new_random_detector)
+        self.detector_list_ = np.array(self.detector_list_)
+        return self
+
+    def predict(self,X):
+
+        """
+
+        :param X: this is an array of the data to be predicted. note if it has only 1 element, then use [element]
+            instead of just passing element.
+        :return: a list containing the predicted class labels for the provided data.
+        """
+
+        #check is fit had been called
+        check_is_fitted(self,'detector_list_')
+
+        #input validation. This will display error message if there is no input
+        #X = check_array(X)
+
+        #array to hold the predicted label
+        predicted_class=[]
+
+        kdt = KDTree(self.detector_list_, leaf_size=30, metric='euclidean')
+
+        for i in range(len(X)):
+            dist, ind = kdt.query([X[i]], k=1)
+#            print 'dist =', dist
+
+            if dist >= self.self_radius_size:
+                predicted_class.append(self.class_label[0])
+            else:
+                predicted_class.append(self.class_label[1])
+
+        return predicted_class
+
+    def score(self, X, y, sample_weight=None):
+        pred_score_ = self.predict(X)
+        return  accuracy_score(y_true=y,y_pred=pred_score_)
+
+
+
+
 
 
 
--- test.py
+++ test.py
@@ -1,18 +1,40 @@
-import pandas as pd
-import mlgrapher as gp
-from sklearn import datasets
+import myclassifiers
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.grid_search import GridSearchCV
+
+mc=myclassifiers.NsaConstantDetectorClassifier(number_of_detectors = 1000, self_radius_size = 0.16,
+                                               random_gen_param = (0.001,1.0),class_label = (1.0,2.0))
+
+my_data = np.genfromtxt('breast_cancer_test_norm.csv', delimiter=',')
+normal = my_data[1:201,1:6]#my_data[1:201,1:6]
+classs_normal=my_data[1:201,6]
+classs_test=my_data[241:,6]#my_data[241:,6]#201:,6
+test_data=my_data[241:,1:6]#my_data[241:,1:6] or 201 #201:,1:6
 
 
 
-#data  = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
-#data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data", header=None)
-data = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data")
-print data
-grapher=gp.Mlgrapher()
+k=mc.fit(normal,classs_normal)
+#X=[[2,3],[3,4]]
+#y=['self','self']
 
-class_column_index =11
-print data.iat[0, class_column_index]
-#grapher.parallel_coord(data,class_column_index,class_color_tuple=('red','blue'))
 
-#print data
-#print len(data)
+#X=np.array(X)
+#y=np.array(y)
+#k=mc.fit(X,y)
+
+#print k
+
+#X1=[[0,40]]
+#X1 = np.array(X1)
+#plt.scatter(y[:,0],y[:,1])
+#plt.show()
+#print mc.predict(test_data, y=(1.0,2.0))
+print mc.score(test_data, classs_test)
+
+tuned_params={'self_radius_size':[i/10.0 for i in range(0,21)]}
+
+gs = GridSearchCV(myclassifiers.NsaConstantDetectorClassifier(),tuned_params)
+
+gs.fit(test_data,classs_test)
+gs.best_params_
