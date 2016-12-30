c23af3b88194569f3d4d22e23eb136c78a671c41
obinna Igbe <obiigbe91@gmail.com>
2016-12-30 05:50:53 +0000

    Delete datautils.py

--- boosting/data/datautils.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def datasetFilenames(datasetName):
-   prefix = os.path.join(os.path.dirname(__file__), datasetName)
-   return prefix + '.train', prefix + '.test'
-
-def vectorize(value, values):
-   return [int(v==value) for v in values]
