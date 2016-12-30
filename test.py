58cde55f0d9f0af639d71992f03d72786f880111
obinna Igbe <obiigbe91@gmail.com>
2016-12-30 05:42:03 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,18 @@
 from myclassifiers_test import testClassifier
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.grid_search import GridSearchCV
-from sklearn.ensemble import AdaBoostClassifier as ada
-from sklearn.metrics import roc_curve, auc
-from scipy import interp
-from sklearn.cross_validation import StratifiedKFold
-from sklearn import preprocessing
-from sklearn.metrics import confusion_matrix
-from sklearn.preprocessing import LabelEncoder
+from myAdaboost import boost
 
-from boosting import boost
-from data import adult
+train = [1,2,3,4,5,6,7,8,9,10]
+ytrain = [ 1 , 1 , 1 ,-1 ,-1, -1,  1,  1,  1, -1]
 
-#train = [(1,1),(2,1),(3,1),(4,-1),(5,-1),(6,-1),(1,1),(7,-1),(8,-1),(9,-1),(10,-1)]
-#test =  [(1,1),(2,1),(3,1),(4,-1),(5,-1),(6,-1),(1,1),(7,-1),(8,-1),(9,-1),(10,-1)]
-
-
-train, test = adult.load()
-print len(train)
 
 weakLearner = testClassifier
-rounds = 20
-h = boost(train, weakLearner, rounds)
-
+print testClassifier(train)
+rounds = 100
+hypothesis,error = boost(train,ytrain,weakLearner,rounds)
+print hypothesis
+print error
+#error_range = np.arange(0,1,0.1)
+#plt.plot(error_range,[0.5,0.4,0.583,0.371,0.222,0.395,0.908,0.692,0.299,0.500])
+#
